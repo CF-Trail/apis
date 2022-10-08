0ec4a056f08e52cb8f4406cb33cfe6fb87a6ae16
CF-Trail <106690765+CF-Trail@users.noreply.github.com>
2022-10-08 14:34:08 +0300

    Update fullapi.lua

--- bss/fullapi.lua
+++ bss/fullapi.lua
@@ -5,8 +5,10 @@ api = {
     windDonate = false,
 }
 
+notifications = loadstring(game:HttpGet("https://raw.githubusercontent.com/Jxereas/UI-Libraries/main/notification_gui_library.lua", true))()
+
 function api:MakeNotification(t,b,tt)
-    notifs.new(t,b,tt)
+    notifications.new(t,b,tt)
 end
 
 for i, v in next, game:GetService("ReplicatedStorage").Morphs:GetChildren() do
