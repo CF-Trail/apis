70c503e4eac61f11165857e5878d8ef6f54ec991
CF-Trail <106690765+CF-Trail@users.noreply.github.com>
2022-10-10 18:53:43 +0300

    Update fullapi.lua

--- bss/fullapi.lua
+++ bss/fullapi.lua
@@ -29,6 +29,63 @@ function api:AntiSleep()
 end)    
 end
 
+function api:WalkTo(destination,walkspeed)
+local PathfindingService = game:GetService("PathfindingService")
+local Players = game:GetService("Players")
+local RunService = game:GetService("RunService")
+
+local path = PathfindingService:CreatePath()
+
+local player = Players.LocalPlayer
+local character = player.Character
+local humanoid = character:WaitForChild("Humanoid")
+humanoid.WalkSpeed = walkspeed
+
+local desti = destination
+
+local waypoints
+local nextWaypointIndex
+local reachedConnection
+local blockedConnection
+
+local function followPath(destination)
+
+    local success, errorMessage = pcall(function()
+		path:ComputeAsync(character.PrimaryPart.Position, destination)
+	end)
+
+	if success and path.Status == Enum.PathStatus.Success then
+
+            waypoints = path:GetWaypoints()
+
+		blockedConnection = path.Blocked:Connect(function(blockedWaypointIndex)
+			if blockedWaypointIndex >= nextWaypointIndex then
+				blockedConnection:Disconnect()
+				followPath(destination)
+			end
+		end)
+
+		if not reachedConnection then
+			reachedConnection = humanoid.MoveToFinished:Connect(function(reached)
+				if reached and nextWaypointIndex < #waypoints then
+					nextWaypointIndex += 1
+					humanoid:MoveTo(waypoints[nextWaypointIndex].Position)
+				else
+					reachedConnection:Disconnect()
+					blockedConnection:Disconnect()
+				end
+			end)
+		end
+		nextWaypointIndex = 2
+		humanoid:MoveTo(waypoints[nextWaypointIndex].Position)
+	else
+		warn("Path not computed!", errorMessage)
+	end
+end
+
+followPath(desti)
+end
+
 for i, v in next, game:GetService("ReplicatedStorage").Morphs:GetChildren() do
     table.insert(api.morphs, v.Name)
 end
