8da28c7c9bc2935267ffcd038ea07d8818b63072
CF-Trail <106690765+CF-Trail@users.noreply.github.com>
2022-10-11 21:23:59 +0300

    Update fullapi.lua

--- bss/fullapi.lua
+++ bss/fullapi.lua
@@ -67,6 +67,23 @@ function api:AntiSleep()
     )
 end
 
+function api:Teleport(cords)
+local plr = game:service"Players".LocalPlayer;
+local tween_s = game:service"TweenService";
+local info = TweenInfo.new(1.5,Enum.EasingStyle.Quad);
+function tp(...)
+local tic_k = tick();
+local params = {...};
+local cframe = CFrame.new(params[1],params[2],params[3]);
+local tween,err = pcall(function()
+local tween = tween_s:Create(plr.Character["HumanoidRootPart"],info,{CFrame=cframe});
+      tween:Play();
+end)
+   if not tween then return err end
+end
+tp(cords)
+end
+
 function api:WalkTo(destination, walkspeed)
     local PathfindingService = game:GetService("PathfindingService")
     local Players = game:GetService("Players")
